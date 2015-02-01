9d77de03d8f0b3708cd3a645b80dab1da29bd4e5
samael <samael.65535@gmail.com>
2015-02-01 18:15:06 +0800

    0.02

--- mdbook.el
+++ mdbook.el
@@ -1,32 +1,58 @@
 (require 'request)
 (require 'json)
-(defun search-book (title)
-  (interactive "s[TAG]: ")
+
+(defun show-books (items)
+  (switch-to-buffer-other-window "*test*")
+  (md-book-mode)
+  (setq buffer-read-only nil)
+  (erase-buffer)
+  (set (make-local-variable 'i) 0)
+  (while (< i (length items))
+	(set (make-local-variable 'book) (elt items i))
+	(insert (propertize (format "[%s]" (assoc-default 'title book)) 'face '(:foreground "SpringGreen")))
+	(insert ":")
+	(insert (propertize (format "<%s>\n" (assoc-default 'alt book)) 'face '(:foreground "DeepSkyBlue")))p
+	(setq i (1+ i))
+	)
+  (setq buffer-read-only t)
+  )
+
+(defun seartch-db-book (title)
+  (interactive "s[TITlE]:")
   (let (
 		(a (cons "q" title))
 		(b (cons "fields" "title,alt"))
 		)
-	(message "A %s" a)
-	(message "B %s" b)
-	(message "C %s" (list a b))
+	(message "%s" (list a b))
 	(request
 	 "https://api.douban.com/v2/book/search"
-	 :params (list a b)
+	 :params (list a b c)
 	 :parser 'json-read
 	 :success (function*
 			   (lambda (&key data &allow-other-keys)
-				 (setq books (assoc-default 'books data))
-				 (setq i 0)
-				 (while (< i (length books))
-				   (setq book (elt books i))
-				   (message "title is %s" (assoc-default 'title book))
-				   (message "url is %s" (assoc-default 'alt book))
-				   (setq i (1+ i))
-				   ))))
-	))
+				 ;;(message "%s" (assoc-default 'books data))
+				 (show-books (assoc-default 'books data))
+				 )))
+	)
+  )
 
-(message "ddd")
-(search-book "三国")
-(provide 'insert-book-url)
-;;---------------------------------------
-;; insert-book-url end here
+(defun select-book ()
+  (interactive)
+  (let ((url (thing-at-point 'line)))
+	(delete-window)
+	(insert url)
+	)
+  ;;(switch-to-buffer-other-window (other-buffer (current-buffer) ))
+  )
+(defvar md-book-mode-map
+  (let ((map (make-sparse-keymap)))
+	;; navigation
+	(define-key map (kbd "RET")             'select-book)
+	map)
+  "Keymap for `md-book-mode'.")
+
+(define-derived-mode md-book-mode markdown-mode nil "list"
+  "Major mode for viewing a list.")
+
+(provide 'md-book)
+
