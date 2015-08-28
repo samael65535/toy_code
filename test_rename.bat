975aa9ecb2cbdbd94e8721b4ae6c39184f777ab8
samael <samael.65535@gmail.com>
2015-08-28 11:20:57 +0800

    再次给小明同学修改程序

--- test_rename.bat
+++ test_rename.bat
@@ -3,18 +3,31 @@ setlocal enabledelayedexpansion
 set /a ext=0
 set /a front=0
 set /a ii=0
+:: 重命名周期
 set /a loopCount=2
-for /l %%i in (1,1,100) do (
-       set /a ext=!ii!%%3+!loopCount!
-       set /a front=!ii!/3+1
+for /l %%i in (1,1,99) do (
+       set /a ext=!ii!%%!loopCount!+!loopCount!
+       set /a front=!ii!/!loopCount!+1
        set /a ii+=1
-       if !front! LEQ 10 (  
-          echo a_!ii!.txt "->" 15JX-JS_00!front!ext!.txt
-          rename a_!ii!.txt 15JX-JS_0!front!_00!ext!.txt
+       if !front! LSS 10 (
+	   	  :: 重命名
+		  if !ii! LSS 10 (
+		  		 echo a_0!ii!.txt "->" 15JX-JS_00!front!_0!ext!.txt
+			     rename a_0!ii!.txt 15JX-JS_00!front!_0!ext!.txt
+		  ) else (
+		  		  echo a_!ii!.txt "->" 15JX-JS_00!front!_0!ext!.txt
+	              rename a_!ii!.txt 15JX-JS_00!front!_0!ext!.txt
+		  )
        ) else (
-          if !front! LEQ 100 (
-		echo a_!ii!.txt "->"15JX-JS_0!front!_00!ext!.txt
-                rename a_!ii!.txt 15JX-JS_0!front!_00!ext!.txt
+          if !front! LSS 100 (
+		        :: 重命名
+		  		if !ii! LSS 10 (
+		  		  	 echo a_0!ii!.txt "->" 15JX-JS_0!front!_0!ext!.txt
+					 rename a_0!ii!.txt 15JX-JS_0!front!_0!ext!.txt
+				) else (
+		  		  echo a_!ii!.txt "->" 15JX-JS_0!front!_0!ext!.txt
+	              rename a_!ii!.txt 15JX-JS_0!front!_0!ext!.txt
+	  		    )
           )
        )
 )
